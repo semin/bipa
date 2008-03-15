9987a33a6063b90d7e3871a46b3f8a574d0650e8
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-03-15 12:27:28 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@56 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- app/models/res_map.rb
+++ app/models/res_map.rb
@@ -1,6 +1,6 @@
 class ResMap < Gloria
-  
+
   set_table_name "ResMap"
-  self.primary_key = "res_id"
-  
+  set_primary_key = "res_id"
+
 end
---
+++ app/models/residue_map.rb
@@ -0,0 +1,6 @@
+class ResidueMap < Gloria
+
+  set_table_name "ResdiueMap"
+  set_primary_key = "res_id"
+
+end
