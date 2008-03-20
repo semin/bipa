ef3e4716c9ebb70de32e197d51cf247cafccf181
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-03-20 10:45:08 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@89 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- test/unit/structure_test.rb
+++ test/unit/structure_test.rb
@@ -2,15 +2,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class StructureTest < Test::Unit::TestCase
   
-  def setup
-    @structure = Structure.new(:pdb_code => "abcd")
-  end
-  
   def test_structure_pdb_code
-    assert_equal "abcd", @structure.pdb_code
+    structure = Structure.new(:pdb_code => "abcd")
+    assert_equal "abcd", structure.pdb_code
   end
-  
-  def test_fails
-    assert true
-  end
+
 end
