99efc9a31c9ac41b3b603390cd32e84e0698cc80
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-03-25 14:25:38 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@145 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

---
+++ test/unit/bipa/interface_test.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class Bipa::InterfaceTest < Test::Unit::TestCase
+  
+  def test_asa_of_residue
+    interface = Interface.new
+  end
+  
+  def test_asa_of_sse
+  end
+end
