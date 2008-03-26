0c48d4022b1c2fda2d07bddcad21cc8fe8ac3cea
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-03-26 18:53:30 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@157 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- test/unit/bipa/structure_test.rb
+++ test/unit/bipa/structure_test.rb
@@ -2,87 +2,89 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class Bipa::StructureTest < Test::Unit::TestCase
 
-  def test_models
-    structure = Bipa::Structure.new(valid_structure_params)
-    model1    = Bipa::Model.new(valid_model_params)
-    model2    = Bipa::Model.new(valid_model_params)
+  context "A Stucture instance" do
+    should "have correct models" do
+      structure = Bipa::Structure.new(valid_structure_params)
+      model1    = Bipa::Model.new(valid_model_params)
+      model2    = Bipa::Model.new(valid_model_params)
     
-    structure.models << model1
-    structure.models << model2
+      structure.models << model1
+      structure.models << model2
 
-    assert structure.save
+      assert structure.save
     
-    assert_equal 2, structure.models.size
-    assert_equal model1, structure.models[0]
-    assert_equal model2, structure.models[1]
-  end 
+      assert_equal 2, structure.models.size
+      assert_equal model1, structure.models[0]
+      assert_equal model2, structure.models[1]
+    end 
   
-  def test_chains
-    structure = Bipa::Structure.new(valid_structure_params)
-    model     = Bipa::Model.new(valid_model_params)
-    chain1    = Bipa::Chain.new(valid_chain_params)
-    chain2    = Bipa::Chain.new(valid_chain_params)
+    should "have correct chains" do
+      structure = Bipa::Structure.new(valid_structure_params)
+      model     = Bipa::Model.new(valid_model_params)
+      chain1    = Bipa::Chain.new(valid_chain_params)
+      chain2    = Bipa::Chain.new(valid_chain_params)
     
-    structure.models << model
-    structure.models.first.chains << chain1
-    structure.models.first.chains << chain2
+      structure.models << model
+      structure.models.first.chains << chain1
+      structure.models.first.chains << chain2
   
-    assert structure.save
+      assert structure.save
     
-    assert_equal 2, structure.models.first.chains.size
-    assert_equal chain1, structure.models.first.chains[0]
-    assert_equal chain2, structure.models.first.chains[1]
+      assert_equal 2, structure.models.first.chains.size
+      assert_equal chain1, structure.models.first.chains[0]
+      assert_equal chain2, structure.models.first.chains[1]
     
-    assert_equal 2, structure.chains.size
-    assert_equal chain1, structure.chains[0]
-    assert_equal chain2, structure.chains[1]
-  end
+      assert_equal 2, structure.chains.size
+      assert_equal chain1, structure.chains[0]
+      assert_equal chain2, structure.chains[1]
+    end
   
-  def test_residues
-    structure = Bipa::Structure.new(valid_structure_params)
-    model     = Bipa::Model.new(valid_model_params)
-    chain     = Bipa::Chain.new(valid_chain_params)
-    residue1  = Bipa::AaResidue.new(valid_residue_params)
-    residue2  = Bipa::AaResidue.new(valid_residue_params)
+    should "have correct residues" do
+      structure = Bipa::Structure.new(valid_structure_params)
+      model     = Bipa::Model.new(valid_model_params)
+      chain     = Bipa::Chain.new(valid_chain_params)
+      residue1  = Bipa::AaResidue.new(valid_residue_params)
+      residue2  = Bipa::AaResidue.new(valid_residue_params)
     
-    structure.models << model
-    structure.models.first.chains << chain
-    structure.models.first.chains.first.residues << residue1
-    structure.models.first.chains.first.residues << residue2
+      structure.models << model
+      structure.models.first.chains << chain
+      structure.models.first.chains.first.residues << residue1
+      structure.models.first.chains.first.residues << residue2
     
-    assert structure.save
+      assert structure.save
     
-    assert_equal 2, structure.models.first.chains.first.residues.size
-    assert_equal residue1, structure.models.first.chains.first.residues[0]
-    assert_equal residue2, structure.models.first.chains.first.residues[1]
+      assert_equal 2, structure.models.first.chains.first.residues.size
+      assert_equal residue1, structure.models.first.chains.first.residues[0]
+      assert_equal residue2, structure.models.first.chains.first.residues[1]
     
-    assert_equal 2, structure.residues.size
-    assert_equal residue1, structure.residues[0]
-    assert_equal residue2, structure.residues[1]
-  end
+      assert_equal 2, structure.residues.size
+      assert_equal residue1, structure.residues[0]
+      assert_equal residue2, structure.residues[1]
+    end
   
-  def test_atoms
-    structure = Bipa::Structure.new(valid_structure_params)
-    model     = Bipa::Model.new(valid_model_params)
-    chain     = Bipa::Chain.new(valid_chain_params)
-    residue   = Bipa::AaResidue.new(valid_residue_params)
-    atom1     = Bipa::Atom.new(valid_atom_params)
-    atom2     = Bipa::Atom.new(valid_atom_params)
+    should "have correct atoms" do
+      structure = Bipa::Structure.new(valid_structure_params)
+      model     = Bipa::Model.new(valid_model_params)
+      chain     = Bipa::Chain.new(valid_chain_params)
+      residue   = Bipa::AaResidue.new(valid_residue_params)
+      atom1     = Bipa::Atom.new(valid_atom_params)
+      atom2     = Bipa::Atom.new(valid_atom_params)
     
-    structure.models << model
-    structure.models.first.chains << chain
-    structure.models.first.chains.first.residues << residue
-    structure.models.first.chains.first.residues.first.atoms << atom1
-    structure.models.first.chains.first.residues.first.atoms << atom2
+      structure.models << model
+      structure.models.first.chains << chain
+      structure.models.first.chains.first.residues << residue
+      structure.models.first.chains.first.residues.first.atoms << atom1
+      structure.models.first.chains.first.residues.first.atoms << atom2
     
-    assert structure.save
+      assert structure.save
     
-    assert_equal 2, structure.models.first.chains.first.residues.first.atoms.size
-    assert_equal atom1, structure.models.first.chains.first.residues.first.atoms[0]
-    assert_equal atom2, structure.models.first.chains.first.residues.first.atoms[1]
+      assert_equal 2, structure.models.first.chains.first.residues.first.atoms.size
+      assert_equal atom1, structure.models.first.chains.first.residues.first.atoms[0]
+      assert_equal atom2, structure.models.first.chains.first.residues.first.atoms[1]
     
-    assert_equal 2, structure.atoms.size
-    assert_equal atom1, structure.atoms[0]
-    assert_equal atom2, structure.atoms[1]
+      assert_equal 2, structure.atoms.size
+      assert_equal atom1, structure.atoms[0]
+      assert_equal atom2, structure.atoms[1]
+    end
   end
 end
--- test/unit/lib/bio_extensions_test.rb
+++ test/unit/lib/bio_extensions_test.rb
@@ -2,30 +2,137 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class BioExtensionsTest < Test::Unit::TestCase
   
-  def setup
-    @pdb_bio = Bio::PDB.new(IO.read(File.dirname(__FILE__) + "/10mh.pdb"))
-  end
+  include Bipa::Constants
+          
+  context "A Bio::PDB instantiated with 10mh.pdb" do
+    
+    setup do
+      @pdb = Bio::PDB.new(IO.read(File.dirname(__FILE__) + "/10mh.pdb"))
+    end
   
-  def test_Bio_PDB_deposition_date
-    assert_equal "10-AUG-1998", @pdb_bio.deposition_date
-  end
+    should "have a correct deposition date" do
+      assert_equal "10-AUG-1998", @pdb.deposition_date
+    end
   
-  def test_Bio_PDB_resolution
-    assert_equal 2.55, @pdb_bio.resolution
-  end
+    should "have a correct resolution" do
+      assert_equal 2.55, @pdb.resolution
+    end
   
-  def test_Bio_PDB_exp_method
-    assert_equal "X-RAY DIFFRACTION", @pdb_bio.exp_method
-  end
+    should "have a correct experimental method" do
+      assert_equal "X-RAY DIFFRACTION", @pdb.exp_method
+    end
   
-  def test_Bio_PDB_Model_aa_chains
-    assert_equal 1, @pdb_bio.models.first.aa_chains.size
-    assert_equal "A", @pdb_bio.models.first.aa_chains.first.id
-  end
-  
-  def test_Bio_PDB_Model_aa_chains
-    assert_equal 2, @pdb_bio.models.first.na_chains.size
-    assert_equal "B", @pdb_bio.models.first.na_chains[0].id
-    assert_equal "C", @pdb_bio.models.first.na_chains[1].id
+    context "having a Bio::PDB::Model instance" do
+
+      should "have correct amino acid chains" do
+        assert_equal 1,   @pdb.models.first.aa_chains.size
+        assert_equal "A", @pdb.models.first.aa_chains.first.id
+      end
+
+      should "have correct nucleic acid chains" do
+        assert_equal 2,   @pdb.models.first.na_chains.size
+        assert_equal "B", @pdb.models.first.na_chains[0].id
+        assert_equal "C", @pdb.models.first.na_chains[1].id
+      end
+    end
+    
+    context "having a Bio::PDB::Chain instance" do
+      
+      should "be true when sending #aa? to amino acid chain" do
+        assert @pdb.models.first.chains[0].aa?
+      end
+      
+      should "be false when sending other than #aa? to amino acid chain" do
+        assert !@pdb.models.first.chains[0].na?
+        assert !@pdb.models.first.chains[0].dna?
+        assert !@pdb.models.first.chains[0].rna?
+        assert !@pdb.models.first.chains[0].hna?
+      end
+      
+      should "be true when sending #na? to NA chain" do
+        assert @pdb.models.first.chains[1].na?
+      end
+      
+      should "be true when sending #dna? to DNA chain" do
+        assert @pdb.models.first.chains[1].dna?
+      end
+      
+      should "be true when sending other than #dna? to DNA chain" do
+        assert !@pdb.models.first.chains[1].aa?
+        assert !@pdb.models.first.chains[1].rna?
+        assert !@pdb.models.first.chains[1].hna?
+      end
+    end
+    
+    context "having a Bio::PDB::Residue instance" do
+      
+      should "be true when sending #aa? to amino acid residues" do
+        AminoAcids::Residues::STANDARD.each do |aa|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(aa)
+          assert residue.aa?
+        end
+      end
+      
+      should "be false when sending other than #aa? to amino acid residues" do
+        AminoAcids::Residues::STANDARD.each do |aa|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(aa)
+          assert !residue.na?
+          assert !residue.dna?
+          assert !residue.rna?
+        end
+      end
+      
+      should "be true when sending #na? to nucleic acid residues" do
+        NucleicAcids::Residues::ALL.each do |na|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(na)
+          assert residue.na?
+        end
+      end
+      
+      should "be false when sending other than #na? to nucleic acid residues" do
+        NucleicAcids::Residues::ALL.each do |na|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(na)
+          assert !residue.aa?
+        end
+      end
+      
+      should "be true when sending #dna? to DNA residues" do
+        NucleicAcids::Dna::Residues::ALL.each do |dna|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(dna)
+          assert residue.dna?
+        end
+      end
+      
+      should "be false when sending other than #dna? to DNA residues" do
+        NucleicAcids::Dna::Residues::ALL.each do |dna|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(dna)
+          assert !residue.aa?
+          assert !residue.rna?
+        end
+      end
+      
+      should "be true when sending #rna? to RNA residues" do
+        NucleicAcids::Rna::Residues::ALL.each do |rna|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(rna)
+          assert residue.rna?
+        end
+      end
+      
+      should "be false when sending other than #rna? to RNA residues" do
+        NucleicAcids::Rna::Residues::ALL.each do |rna|
+          residue = Bio::PDB::Residue.new
+          residue.stubs(:resName).returns(rna)
+          assert !residue.aa?
+          assert !residue.dna?
+        end
+      end
+    end
   end
 end
