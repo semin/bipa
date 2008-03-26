452c821430273ee3d6233909da43cae659c11268
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-03-26 11:27:50 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@152 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- test/unit/lib/bio_extensions_test.rb
+++ test/unit/lib/bio_extensions_test.rb
@@ -1,4 +1,16 @@
-require File.dirname(__FILE__) + '/../../../test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
-class Bipa::BioExtensionsTest << Test::Unit::TestCase
+class BioExtensionsTest < Test::Unit::TestCase
+  
+  def setup
+    @pdb_bio = Bio::PDB.new(IO.read("./10mh.pdb"))
+  end
+  
+  def test_Bio_PDB_deposition_date
+    assert_equal "1999-10-10", @pdb_bio.deposition_date
+  end
+  
+  def test_Bio_PDB_resolution
+    assert_equal 2.5, @pdb_bio.resolution
+  end
 end
