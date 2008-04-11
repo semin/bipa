906e4daa51a3a503d998ae364b70719cfb179fc0
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-04-11 11:21:01 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@307 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- lib/array_extensions.rb
+++ lib/array_extensions.rb
@@ -1,7 +1,9 @@
 module ArrayExtensions
+  
   def to_stats_array
     Bipa::StatsArray.new(self)
   end
+  
 end
 
 Array.send(:include, ArrayExtensions)
--- lib/vector_extensions.rb
+++ lib/vector_extensions.rb
@@ -1,7 +1,9 @@
 module VectorExtensions
+  
   def manhattan_distance_to(other)
     (self-other).map {|e| e.abs}.to_a.inject(0) {|s, t| s + t}
   end
+  
 end
 
 Vector.send :include, VectorExtensions
