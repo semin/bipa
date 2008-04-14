fe5cf8b96a6bcd1de821ce11ef768fa968aece6e
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-04-14 14:40:38 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@322 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- test/unit/subfamily_test.rb
+++ test/unit/subfamily_test.rb
@@ -9,10 +9,11 @@ class SubfamilyTest < Test::Unit::TestCase
     should "return correct representative when sending #representative" do
       
       sub_fam = Subfamily.new
-      domain1 = stub(:calpha_only? => false,  :resolution => 2.5)
-      domain2 = stub(:calpha_only? => true,   :resolution => 1.0)
-      domain3 = stub(:calpha_only? => false,  :resolution => 3.0)
-      sub_fam.stubs(:domains).returns([domain1, domain2, domain3])
+      domain1 = stub(:calpha_only? => false,  :has_unks? => false,  :resolution => 2.5)
+      domain2 = stub(:calpha_only? => true,   :has_unks? => false,  :resolution => 1.0)
+      domain3 = stub(:calpha_only? => false,  :has_unks? => false,  :resolution => 3.0)
+      domain4 = stub(:calpha_only? => false,  :has_unks? => true,   :resolution => 0.9)
+      sub_fam.stubs(:domains).returns([domain1, domain2, domain3, domain4])
       
       assert_equal domain1, sub_fam.representative
     end
@@ -29,3 +30,4 @@ end
     end
   END
 end
+
