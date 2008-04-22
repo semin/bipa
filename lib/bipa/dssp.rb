94dbdfc270d839dbf8ee470735f73a49a554e066
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-04-22 16:43:03 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@368 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- lib/bipa/dssp.rb
+++ lib/bipa/dssp.rb
@@ -1,31 +1,78 @@
 module Bipa
   
   class Dssp
-    attr_reader :residues
+    Dssp = Struct.new(:dssp_number,
+                      :residue_number,
+                      :icode,
+                      :chain_id,
+                      :residue_name,
+                      :sse,
+                      :three_turns,
+                      :four_turns,
+                      :five_turns,
+                      :geometrical_bend,
+                      :chirality,
+                      :beta_bridge_label_1,
+                      :beta_bridge_label_2,
+                      :beta_brdige_partner_residue_number_1,
+                      :beta_brdige_partner_residue_number_2,
+                      :beta_sheet_label,
+                      :solvent_accessibility,
+                      :nh_o_hbond_1_acceptor,
+                      :nh_o_hbond_1_energy,
+                      :o_hn_hbond_1_donor,
+                      :o_hn_hbond_1_energy,
+                      :nh_o_hbond_2_acceptor,
+                      :nh_o_hbond_2_energy,
+                      :o_hn_hbond_2_donor,
+                      :o_hn_hbond_2_energy,
+                      :tco,
+                      :kappa,
+                      :alpha,
+                      :phi,
+                      :psi)
 
     def initialize(dssp_str)
-      @residues = {}
+      dssp = {}
 
       dssp_str.each_line do |line|
         next if line =~ /^\s*#/
-        if line.length == 137
-          resnum      = line[5..9].strip
-          icode       = line[10..10].strip
-          chainid     = line[11..11].strip
-          resname     = line[13..14].strip
-          sse         = line[16..16].strip
-          three_turns = line[18..18].strip
-          four_turns  = line[19..19].strip
-          five_turns  = line[20..20].strip
-          geo_bend    = line[21..21].strip
-          chirality   = line[22..22].strip
-          beta_bridge_label1 = line[23..23].strip
-          beta_bridge_label2 = line[24..24].strip
-          
+        if line.length == 137 && !line[5..9].strip.blank?
+          dssp = Dssp.new(line[0..4].strip,
+                          line[5..9].strip,
+                          line[10..10].strip, 
+                          line[11..11].strip, 
+                          line[13..14].strip, 
+                          line[16..16].strip, 
+                          line[18..18].strip, 
+                          line[19..19].strip, 
+                          line[20..20].strip, 
+                          line[21..21].strip, 
+                          line[22..22].strip, 
+                          line[23..23].strip, 
+                          line[24..24].strip, 
+                          line[26..28].strip, 
+                          line[30..32].strip, 
+                          line[33..33].strip, 
+                          line[34..37].strip, 
+                          line[39..44].strip, 
+                          line[46..49].strip, 
+                          line[51..55].strip, 
+                          line[57..60].strip, 
+                          line[62..66].strip, 
+                          line[68..71].strip, 
+                          line[73..77].strip, 
+                          line[79..82].strip, 
+                          line[84..90].strip, 
+                          line[91..96].strip, 
+                          line[97..102].strip,
+                          line[103..108].strip,
+                          line[109..114].strip)
 
-          @residues["#{res_chain}#{res_number}#{res_icode}"] = res_sstruc
+          @residues[line[5..11].gsub(/\s+/, '')] = dssp
         end
       end
+      dssp
     end
   end
   
