ba2de43fdb5cf89925f4161cd0e43d7a58746bae
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-04-27 11:24:36 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@390 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- lib/bipa/dssp.rb
+++ lib/bipa/dssp.rb
@@ -1,3 +1,12 @@
+require "active_support"
+
+class String
+  def nil_if_blank
+    self.blank? ? nil : self
+  end
+end
+
+
 class Struct
   def to_hash
     hash = Hash.new
@@ -48,31 +57,31 @@ module Bipa
         next if line =~ /^\s*#/
         if line.length == 137 && !line[5..9].strip.blank?
           dssp = DsspLine.new(line[0..4].strip.to_i,      #dssp_number
-                              line[16..16].strip,         #sse
-                              line[18..18].strip,         #three_turns
-                              line[19..19].strip,         #four_turns
-                              line[20..20].strip,         #five_turns
-                              line[21..21].strip,         #geometrical_bend
-                              line[22..22].strip,         #chirality
-                              line[23..23].strip,         #beta_bridge_label_1
-                              line[24..24].strip,         #beta_bridge_label_2
-                              line[26..28].strip.to_i,    #beta_brdige_partner_residue_number_1
-                              line[30..32].strip.to_i,    #beta_brdige_partner_residue_number_2
-                              line[33..33].strip,         #beta_sheet_label
-                              line[34..37].strip.to_f,    #sasa
-                              line[39..44].strip.to_i,    #nh_o_hbond_1_acceptor
-                              line[46..49].strip.to_f,    #nh_o_hbond_1_energy
-                              line[51..55].strip.to_i,    #o_hn_hbond_1_donor
-                              line[57..60].strip.to_f,    #o_hn_hbond_1_energy
-                              line[62..66].strip.to_i,    #nh_o_hbond_2_acceptor
-                              line[68..71].strip.to_f,    #nh_o_hbond_2_energy
-                              line[73..77].strip.to_i,    #o_hn_hbond_2_donor
-                              line[79..82].strip.to_f,    #o_hn_hbond_2_energy
-                              line[84..90].strip.to_f,    #tco
-                              line[91..96].strip.to_f,    #kappa
-                              line[97..102].strip.to_f,   #alpha
-                              line[103..108].strip.to_f,  #phi
-                              line[109..114].strip.to_f)  #psi
+                              line[16..16].strip.blank? ? "C" : line[16..16].strip,         #sse
+                              line[18..18].strip.nil_if_blank,  #three_turns
+                              line[19..19].strip.nil_if_blank,  #four_turns
+                              line[20..20].strip.nil_if_blank,  #five_turns
+                              line[21..21].strip.nil_if_blank,  #geometrical_bend
+                              line[22..22].strip.nil_if_blank,  #chirality
+                              line[23..23].strip.nil_if_blank,  #beta_bridge_label_1
+                              line[24..24].strip.nil_if_blank,  #beta_bridge_label_2
+                              line[26..28].strip.to_i,          #beta_brdige_partner_residue_number_1
+                              line[30..32].strip.to_i,          #beta_brdige_partner_residue_number_2
+                              line[33..33].strip.nil_if_blank,  #beta_sheet_label
+                              line[34..37].strip.to_f,          #sasa
+                              line[39..44].strip.to_i,          #nh_o_hbond_1_acceptor
+                              line[46..49].strip.to_f,          #nh_o_hbond_1_energy
+                              line[51..55].strip.to_i,          #o_hn_hbond_1_donor
+                              line[57..60].strip.to_f,          #o_hn_hbond_1_energy
+                              line[62..66].strip.to_i,          #nh_o_hbond_2_acceptor
+                              line[68..71].strip.to_f,          #nh_o_hbond_2_energy
+                              line[73..77].strip.to_i,          #o_hn_hbond_2_donor
+                              line[79..82].strip.to_f,          #o_hn_hbond_2_energy
+                              line[84..90].strip.to_f,          #tco
+                              line[91..96].strip.to_f,          #kappa
+                              line[97..102].strip.to_f,         #alpha
+                              line[103..108].strip.to_f,        #phi
+                              line[109..114].strip.to_f)        #psi
 
           @residues[line[5..11].gsub(/\s+/, '')] = dssp
         end
