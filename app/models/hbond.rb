474e2b75d7334d9932c12b4ad12fd7472e7b8e88
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-04-30 11:50:04 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@412 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

---
+++ app/models/hbond.rb
@@ -0,0 +1,10 @@
+class Hbond < ActiveRecord::Base
+
+  belongs_to  :donor,
+              :class_name   => "Atom",
+              :foreign_key  => "donor_id"
+
+  belongs_to  :acceptor,
+              :class_name   => "Atom",
+              :foreign_key  => "acceptor_id"
+end
