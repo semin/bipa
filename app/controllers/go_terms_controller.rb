f8a1d06f3dcc085bfe2467c1277a79648211e361
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-05-17 18:47:58 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@506 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

---
+++ app/controllers/go_terms_controller.rb
@@ -0,0 +1,18 @@
+class GoTermsController < ApplicationController
+
+  def chilren
+    children = GoTerm.registered.find(params[:root]).  registered_children
+
+    children.each do |child|
+      child[:tree_title] = child.tree_title
+
+      if child.registered_children.size > 0
+        child[:hasChildren] = true
+      end
+    end
+
+    respond_to do |format|
+      format.json { render :json => children.to_json }
+    end
+  end
+end
