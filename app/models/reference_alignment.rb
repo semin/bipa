bbf14cda168f30c2aa79ff55be5315a64d45fe3f
semin <semin@1ce2efd8-ef97-11dc-b254-1d548ba94f2d>
2008-10-07 08:26:10 +0000

    git-svn-id: svn+ssh://mordred.bioc.cam.ac.uk/home/svn/bipa/trunk@756 1ce2efd8-ef97-11dc-b254-1d548ba94f2d

--- app/models/reference_alignment.rb
+++ app/models/reference_alignment.rb
@@ -20,11 +20,6 @@ class ReferenceAlignment < ActiveRecord::Base
 
   has_one :test_na_fugue_alignment
 
-  named_scope :pid1_0_20,   :conditions => ["pid1 >= 0 and pid1 < 20"]
-  named_scope :pid1_20_30,  :conditions => ["pid1 >= 20 and pid1 < 30"]
-  named_scope :pid1_30_40,  :conditions => ["pid1 >= 30 and pid1 < 40"]
-  named_scope :pid1_40_100, :conditions => ["pid1 >= 40 and pid1 <= 100"]
-
   named_scope :pid_range,   lambda { |*args|
     { :conditions => ["pid#{args[0]} > ? and pid#{args[0]} <= ?", args[1], args[2]] }
   }
