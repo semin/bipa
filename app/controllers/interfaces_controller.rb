b553191515b2b8d157264fc9c7af3666a9935897
Semin Lee <seminlee@gmail.com>
2008-12-04 15:18:54 +0000

    structures_controller.rb updated

--- app/controllers/interfaces_controller.rb
+++ app/controllers/interfaces_controller.rb
@@ -2,8 +2,6 @@ class InterfacesController < ApplicationController
 
   caches_page :show
 
-  before_filter :find_scop, :only => :index
-
   def index
     @interfaces = DomainInterface.all.paginate(:page => params[:page] || 1, :per_page => 10)
 
@@ -21,12 +19,4 @@ class InterfacesController < ApplicationController
     end
   end
 
-  private
-
-  def find_scop
-    @scop_id = params[:scop_id]
-
-    redirect_to :back unless @scop_id
-    @scop = Scop.find(@scop_id)
-  end
 end
--- app/controllers/structures_controller.rb
+++ app/controllers/structures_controller.rb
@@ -11,8 +11,6 @@ class StructuresController < ApplicationController
     end
   end
 
-  # GET /structures/1
-  # GET /structures/1.xml
   def show
     @structure  = Structure.find(params[:id])
 
@@ -30,59 +28,4 @@ class StructuresController < ApplicationController
     end
   end
 
-#  # GET /structures/new
-#  def new
-#    @structure = Structure.new
-#  end
-
-#  # GET /structures/1;edit
-#  def edit
-#    @structure = Structure.find(params[:id])
-#  end
-
-#  # POST /structures
-#  # POST /structures.xml
-#  def create
-#    @structure = Structure.new(params[:structure])
-#
-#    respond_to do |format|
-#      if @structure.save
-#        flash[:notice] = 'Structure was successfully created.'
-#        format.html { redirect_to structure_url(@structure) }
-#        format.xml  { head :created, :location => structure_url(@structure) }
-#      else
-#        format.html { render :action => "new" }
-#        format.xml  { render :xml => @structure.errors.to_xml }
-#      end
-#    end
-#  end
-
-#  # PUT /structures/1
-#  # PUT /structures/1.xml
-#  def update
-#    @structure = Structure.find(params[:id])
-#
-#    respond_to do |format|
-#      if @structure.update_attributes(params[:structure])
-#        flash[:notice] = 'Structure was successfully updated.'
-#        format.html { redirect_to structure_url(@structure) }
-#        format.xml  { head :ok }
-#      else
-#        format.html { render :action => "edit" }
-#        format.xml  { render :xml => @structure.errors.to_xml }
-#      end
-#    end
-#  end
-
-#  # DELETE /structures/1
-#  # DELETE /structures/1.xml
-#  def destroy
-#    @structure = Structure.find(params[:id])
-#    @structure.destroy
-#
-#    respond_to do |format|
-#      format.html { redirect_to structures_url }
-#      format.xml  { head :ok }
-#    end
-#  end
 end
