2771d78b5ac035a81c2bf522dbaeb51f7ae3248a
Semin <semin@mordred.bioc.cam.ac.uk>
2009-02-19 15:02:30 +0000

    home.html.erb page now serves static info

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,23 +1,23 @@
 class MainController < ApplicationController
 
-  caches_page :home, :contact
+  caches_page :home
 
   def home
     @structure    = Structure.latest.first
     @latest_news  = News.order('date desc').limit(5)
 
-    %w[dna rna].each do |na|
-      eval <<-RUBY_CODE
-        @#{na}_interfaces ||= ScopDomain.rp80_#{na}.map { |d| d.#{na}_interfaces }.flatten.compact
-      RUBY_CODE
-
-      %w[asa polarity hbonds_count whbonds_count vdw_contacts_count].each do |property|
-        eval <<-RUBY_CODE
-          @mean_#{na}_interface_#{property} ||= @#{na}_interfaces.map(&:#{property}).to_stats_array.mean
-          @stddev_#{na}_interface_#{property} ||= @#{na}_interfaces.map(&:#{property}).to_stats_array.stddev
-        RUBY_CODE
-      end
-    end
+#    %w[dna rna].each do |na|
+#      eval <<-RUBY_CODE
+#        @#{na}_interfaces ||= ScopDomain.rp80_#{na}.map { |d| d.#{na}_interfaces }.flatten.compact
+#      RUBY_CODE
+#
+#      %w[asa polarity hbonds_count whbonds_count vdw_contacts_count].each do |property|
+#        eval <<-RUBY_CODE
+#          @mean_#{na}_interface_#{property} ||= @#{na}_interfaces.map(&:#{property}).to_stats_array.mean
+#          @stddev_#{na}_interface_#{property} ||= @#{na}_interfaces.map(&:#{property}).to_stats_array.stddev
+#        RUBY_CODE
+#      end
+#    end
 
     respond_to do |format|
       format.html
