bf63baa8c492dba0f7897e8b22268615de7943a3
Semin <semin@mordred.bioc.cam.ac.uk>
2009-05-27 22:05:37 +0100

    import_with_load_data_in_file removed

--- vendor/plugins/import_with_load_data_in_file/test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
-require 'test/unit'
-# gem install redgreen for colored test output
-begin require 'redgreen'; rescue LoadError; end
